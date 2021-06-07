ad3585645478186786d58c9c78dbd85bdce65d83
rahmatullah <rahmatullahabror@gmail.com>
2021-06-07 20:34:30 +0700

    mongoDB

--- main.py
+++ main.py
@@ -1,29 +1,4 @@
-from fastapi import FastAPI
+import uvicorn
 
-# from enum import Enum
-
-# class ModelName(str, Enum) :
-#     alexnet = "alexnet"
-#     resnet = "resnet"
-#     lenet = "lenet"
-
-app = FastAPI()
-
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
-
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
-
-# @app.get("/models/{model_name}")
-# async def get_model(model_name: ModelName):
-#     if model_name == ModelName.alexnet:
-#         return {"model_name" : model_name, "message" : "Deep Learning FTW!"}
-#     if model_name.value == "lenet":
-#         return {"model_name" : model_name, "message" : "LeCNN all the images"}
-#     return {"model_name" : model_name, "message" : "LeCNN all the images"}
-
-# @app.get("/users/me")
-# async def read_user_me():
-#     return {"user_id": "the current user"}
-
+if __name__ == "__main__":
+    uvicorn.run("server.app:app", host="0.0.0.0", port=8000, reload=True)
