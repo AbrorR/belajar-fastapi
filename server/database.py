77e999b3d7d693fbee6e3b7b4c056e75581629dd
rahmatullah <rahmatullahabror@gmail.com>
2021-06-18 16:09:33 +0700

    jwt token

--- server/database.py
+++ server/database.py
@@ -42,8 +42,30 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 SECRET_KEY = "d849550a56736ecafa159d5b68e5bd166fd8c7cf96377b804c04e0693de42dab"
 ALGORITHM = "HS256"
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
-    print(token)
+@app.get("/users")
+def get_current_user(token = Depends (oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub") 
+        if username is None:
+            raise credentials_exception
+        return username
+    except JWTError:
+        raise credentials_exception
+    # user = user_collection.find_one({"username":token_data.username})
+    # if user is None:
+    #     raise credentials_exception
+    # return user
+
+# async def get_current_active_user(current_user: user_collection = Depends(get_current_user)):
+#     if current_user.disabled:
+#         raise HTTPException(status_code=400, detail="Inactive user")
+#     return current_user
 
 def create_access_token(data: dict):
     to_encode = data.copy()
@@ -73,6 +95,7 @@ async def login_user(form_data: OAuth2PasswordRequestForm = Depends()):
 
 @app.get("/items")
 def items(current_user = Depends(get_current_user)):
+    print(current_user)
     return {"oke"}
 
 # Retrieve all
